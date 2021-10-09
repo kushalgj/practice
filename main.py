45cb3175668f270be4c1f18750ac0aa90ad71f50
kushalgj <92223211+kushalgj@users.noreply.github.com>
2021-10-09 21:44:40 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def sum_of_num():
+    first_num = int(input("Enter first number : "))
+    second_num = int(input("Enter second number : "))
+    result = first_num + second_num
+    print("Sum of two numbers : ", result)
+
+sum_of_num()
+print("this is my first commit")
+
+
